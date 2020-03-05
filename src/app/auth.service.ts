92cc326412c61ec05f74c41ba149a06ef6c6ddfa
Ryan Noronha <ryannoronha@Ryans-MacBook-Pro.local>
2020-03-05 15:06:16 -0300

    fix logout bug

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -5,6 +5,7 @@ import { Injectable } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AppUser } from './models/app-user';
 import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/observable/of';
 import * as firebase from 'firebase';
 
 @Injectable({
@@ -33,6 +34,10 @@ export class AuthService {
 
   get appUser$() : Observable<AppUser> {
     return this.user$
-      .switchMap(user => this.userService.get(user.uid))
+      .switchMap(user => {
+        if (user) return this.userService.get(user.uid);
+
+        return Observable.of(null);
+      });
   }
 }
